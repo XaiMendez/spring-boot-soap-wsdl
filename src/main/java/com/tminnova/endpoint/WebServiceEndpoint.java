cdd1c0c87734a1b3f9134a310f8f1b5c9a19f909
Xavier <shavy_00@hotmail.com>
2018-07-16 23:00:13 -0600

    fixing files

---
+++ src/main/java/com/tminnova/endpoint/WebServiceEndpoint.java
@@ -0,0 +1,29 @@
+package com.tminnova.endpoint;
+
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+import com.tminnova.wsdl.InputSOATest;
+import com.tminnova.wsdl.ObjectFactory;
+import com.tminnova.wsdl.OutputSOATest;
+
+@Endpoint
+public class WebServiceEndpoint {
+
+	private static final String NAMESPACE_URI = "http://javainuse.com";
+
+	@PayloadRoot(namespace = NAMESPACE_URI, localPart = "inputSOATest")
+	@ResponsePayload
+	public OutputSOATest hello(@RequestPayload InputSOATest request) {
+
+		String outputString = "Hello " + request.getTest() + "!";
+
+		ObjectFactory factory = new ObjectFactory();
+		OutputSOATest response = factory.createOutputSOATest();
+		response.setResult(outputString);
+
+		return response;
+	}
+}
