dd3a23fe6c793bc742993a34734e26a6d683fbc3
Xavier <shavy_00@hotmail.com>
2018-07-16 22:31:54 -0600

    adding initial files

---
+++ src/main/java/com/tminnova/SpringBootSoapWsdlApplication.java
@@ -0,0 +1,12 @@
+package com.tminnova;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSoapWsdlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSoapWsdlApplication.class, args);
+	}
+}
